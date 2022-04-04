a8461875ffc446c02ec01b22e859a92448af7bb5
Peter01Git <peterbach2001@gmail.com>
2022-04-04 21:59:23 +0200

    the idea is finish

--- src/Clock/Main.java
+++ src/Clock/Main.java
@@ -1,7 +1,33 @@
 package Clock;
 
-public class Main {
+public class Main implements Runnable {
+	private Thread thread = null;
+	int time;
+	
+	//constructor
+	public Main() {
+		thread = new Thread(this);
+		thread.start();
+		time = 0;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			while(true) {
+				time++;
+				if(time == 60) {
+					time = 0;
+				}
+				System.out.println(time);
+				Thread.sleep(100);
+			}
+		} catch (Exception e) {
+			
+		}		
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("test1");
+		new Main();
 	}
 }
